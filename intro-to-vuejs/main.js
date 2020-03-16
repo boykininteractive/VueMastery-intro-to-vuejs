8554b5ecb9cf60545d8ca53d4a7b5e8d060583b2
boykintj <boykintj@boykintjs-mbp.lan>
2020-03-16 16:27:43 -0400

    Use v-on to handle Events
        <button @click="addToCart">Add to cart</button>
        <div @mouseover="updateProduct">Color</div>
        <form @submit="addToCart">...</form>
        <input @keyup.enter="send">

--- intro-to-vuejs/main.js
+++ intro-to-vuejs/main.js
@@ -2,21 +2,34 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    image: "./assets/vmSocks-blue-onWhite.jpg",
     link: "https://vuemastery.com",
+    image: "./assets/vmSocks-blue-onWhite.jpg",
     inStock: true,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    sizes: ["S", "M", "L", "XL", "XXL"],
+    cart: 0,
     variants: [
       {
         variantID: 2234,
-        variantColor: "green"
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg"
       },
       {
         variantID: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
       }
     ]
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+    },
+    updateProduct(color) {
+      this.image = color;
+    }
   }
 });
