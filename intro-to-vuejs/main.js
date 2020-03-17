9ab73fe8a28e0d2f518b3574edcfb0d46773c966
boykintj <boykintj@boykintjs-mbp.lan>
2020-03-16 22:45:15 -0400

    Add form for reviews

--- intro-to-vuejs/main.js
+++ intro-to-vuejs/main.js
@@ -1,3 +1,79 @@
+Vue.component("product-review", {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+    <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+            <li v-for="error in errors">{{error}}</li>
+        </ul>
+    </p>
+
+    <p>
+        <label for="name">Name:</label>  
+        <input id="name" v-model="name">
+    </p>
+ 
+    <p>
+        <label for="review">Review:</label>  
+        <textarea id="review" v-model="review" ></textarea>
+    </p>
+
+    <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+        </select>
+    </p>
+
+        <div>Would you recommend this product?</div>
+        <p>
+        <label for="recommend">Yes</label>
+            <input v-model="recommend" type="radio" id="recommend" name="recommend" value="Yes">
+        <label for="recommend">No</label>
+            <input v-model="recommend" type="radio" id="recommend" name="recommend" value="No">
+    </p>
+
+    <p>
+        <input type="submit" value="Submit">
+    </p>
+    </form>
+    `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      recommend: null,
+      errors: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.errors = [];
+      if (this.name && this.review && this.rating && this.recommend) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+          recommend: this.recommend
+        };
+        this.$emit("review-submitted", productReview)((this.name = null))(
+          (this.review = null)
+        )((this.rating = null));
+      } else {
+        if (!this.name) this.errors.push("Name required.");
+        if (!this.review) this.errors.push("Review required.");
+        if (!this.rating) this.errors.push("Rating required.");
+        if (!this.recommend) this.errors.push("Recommendation required.");
+      }
+    }
+  }
+});
+
 Vue.component("product", {
   props: {
     premium: {
@@ -38,7 +114,22 @@ Vue.component("product", {
                 </button>
       
              </div>  
-          
+
+             <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p>{{ review.name}}</p>
+                        <p>Rating: {{ review.rating}}</p>
+                        <p>{{ review.review}}</p>
+                        <p>Would you recommend? {{ review.recommend}}</p>
+                    </li>
+                </ul>
+            </div>
+             
+             <product-review @review-submitted="addReview"></product-review>
+
           </div>
          `,
   data() {
@@ -60,7 +151,8 @@ Vue.component("product", {
           variantImage: "./assets/vmSocks-blue-onWhite.jpg",
           variantQuantity: 0
         }
-      ]
+      ],
+      reviews: []
     };
   },
   methods: {
@@ -69,6 +161,9 @@ Vue.component("product", {
     },
     updateProduct(index) {
       this.selectedVariant = index;
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     }
   },
   computed: {
